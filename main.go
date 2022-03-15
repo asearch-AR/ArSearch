718a937a6aa1ae9b11d25b39893299cbf277157f
guruiqin <guruiqin@zhihu.com>
2022-03-15 15:56:58 +0800

    add

--- main.go
+++ main.go
@@ -14,5 +14,5 @@ func main() {
 	})
 
 	r.GET("/query",controller.SearchData)
-	r.Run(":9999") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run("0.0.0.0:9999") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
